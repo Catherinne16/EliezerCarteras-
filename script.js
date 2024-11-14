fd34e47cf39f5937ff1bdc283b4758de35baa709
JennyBerrys <jennifer.diaz@genstudents.org>
2024-11-14 18:13:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let currentIndex = 0;
+
+const images = document.querySelectorAll(".carousel-images img");
+const carouselImages = document.querySelector(".carousel-images");
+const prevButton = document.querySelector(".carousel-button.prev");
+const nextButton = document.querySelector(".carousel-button.next");
+
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    const offset = -currentIndex * 100;
+    carouselImages.style.transform = `translateX(${offset}%)`;
+}
+
+function showPrevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length; // Asegura que no se salga del rango
+    const offset = -currentIndex * 100;
+    carouselImages.style.transform = `translateX(${offset}%)`;
+}
+
+// Manejadores de clic para los botones
+nextButton.addEventListener('click', showNextImage);
+prevButton.addEventListener('click', showPrevImage);
+
+// Cambiar imagen cada 3 segundos (automático)
+setInterval(showNextImage, 3000);
